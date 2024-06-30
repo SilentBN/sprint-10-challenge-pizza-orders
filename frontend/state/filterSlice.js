1d6362588ea427822b440160524a13ece7f214cb
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-06-30 14:18:10 -0400

    Created Filter Slice

---
+++ frontend/state/filterSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit"; // imports the createSlice function from the Redux Toolkit library
+
+// Creates a slice for filter state
+const filterSlice = createSlice({
+  name: "filter", // The name of the slice
+  initialState: "All", // The initial state of the slice
+  // Define Reducer Functions
+  reducers: {
+    // This defines a reducer function named setFilter: state is the current state (in this case, the current filter value) action is the action object dispatched to this reducer action.payload contains the new filter value The function simply returns the new filter value, replacing the old one
+    setFilter: (state, action) => {
+      return action.payload;
+    },
+  },
+});
+
+export const { setFilter } = filterSlice.actions; // This exports the setFilter action creator. createSlice automatically generates action creators based on the reducer functions we defined.
+export default filterSlice.reducer; // This exports the reducer function for our slice. This reducer will handle all actions related to the filter state.
